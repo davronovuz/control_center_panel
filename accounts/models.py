f28e9377741a68675bcd54ea147516df03f33d09
davronovuz <davronovtatu@gmail.com>
2026-01-18 21:55:06 +0500

    sasasasas

--- accounts/models.py
+++ accounts/models.py
@@ -267,7 +267,7 @@ class User(AbstractUser):
 
     # -------- CONTACT --------
     phone = models.CharField(
-        max_length=13,
+        max_length=20,
         validators=[phone_validator],
         unique=True,
         null=True,
